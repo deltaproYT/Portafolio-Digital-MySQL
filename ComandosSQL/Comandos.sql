1a77d7d0d279ff900e36a49248b5d028be67a739
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2025-09-26 09:29:19 -0500

    Subida inicial de proyectos MySQL

---
+++ ComandosSQL/Comandos.sql
@@ -0,0 +1,41 @@
+-- SELECT --
+SELECT * FROM users; --Selecciona TODAS (*) las columnas de la database 'users' --
+
+SELECT idusers, username FROM users; -- Selecciona las columnas 'idusers' y 'username' de la database 'users' --
+
+
+-- SELECT DISTINCT --
+SELECT DISTINCT email FROM users; -- Selecciona solo los valores sin repetir de la columna 'email' de la database 'users' --
+
+
+-- SELECT WHERE --
+SELECT * FROM users WHERE email = 'Gmail.com'; -- Selecciona todas las columnas del database si el dato en la columna email es 'Gmail' --
+
+
+-- ORDER BY --
+SELECT * FROM users ORDER BY email; -- Selecciona TODAS las columnas de la database 'users' y las ordena de manera ascendente (por defecto) --
+
+SELECT * FROM users ORDER BY email ASC; -- Selecciona TODAS las columnas de la database 'users' y las ordena de manera ascendente --
+
+SELECT * FROM users ORDER BY email DESC; -- Selecciona TODAS las columnas de la database 'users' y las ordena de manera descendente --
+
+-- WHERE ORDER BY --
+SELECT * FROM users WHERE email = 'Gmail.com' ORDER BY last_name; -- Selecciona TODAS las columnas de la database donde email sea igual a 'Gmail' y las ordena por apellido de forma ascendente (por defecto) --
+
+
+-- WHERE LIKE --
+SELECT * FROM users WHERE email LIKE 'Gmail.com'; -- Selecciona todas las columnas del database 'users' si el dato en la columna email es parecido a 'Gmail' --
+
+SELECT * FROM users WHERE email LIKE '%net'; -- Selecciona TODAS las columnas de la database 'users' si el dato en la columna email tiene terminacion 'net' --
+
+SELECT * FROM users WHERE first_name LIKE 'Y%'; -- Selecciona TODAS las columnas de la database 'users' si el dato en la columna first_name empieza con 'y' --
+
+SELECT * FROM users WHERE birth_date LIKE '%-07-%'; -- Selecciona TODAS las columnas de la database 'users' si el dato en la columna birth_date tiene el mes '07' --
+
+
+-- COMPUERTAS LOGICAS: AND - OR - NOT --
+SELECT * FROM users WHERE NOT birth_date LIKE '%-07-%'; -- Selecciona TODAS las columnas de la database 'users' si el dato en la columna birth_date NO tiene el mes '07' --
+
+SELECT * FROM users WHERE first_name LIKE 'Y%' AND last_name LIKE 'C%'; -- Selecciona TODAS las columnas de la database 'users' si el dato en la columna first_name empieza con 'Y' Y el dato en la columna last-name empiece con 'C'--
+
+SELECT * FROM users WHERE email LIKE '%net' OR email LIKE '%arg'; -- Selecciona TODAS las columnas de la database 'users' si el dato en la columna email tiene terminación 'net' O con la terminación 'arg'--
---
+++ README.md
@@ -0,0 +1,11 @@
+# Proyectos de Programacion MySQL
+
+## Descripción
+Carpeta con programas creados para demostrar mi nivel de aprendizaje dentro del lenguaje de programacion MySQL
+
+## Autor
+Jordy Wladimir Ortiz Aquino
+
+## Contacto
+jordanopro42@gmail.com
+
