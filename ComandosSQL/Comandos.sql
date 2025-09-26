92c54052e5dda2fc0ca764faa11fc9ef82994f2f
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2025-09-26 10:08:55 -0500

    Actualizacion de Repo

--- ComandosSQL/Comandos.sql
+++ ComandosSQL/Comandos.sql
@@ -39,3 +39,27 @@ SELECT * FROM users WHERE NOT birth_date LIKE '%-07-%'; -- Selecciona TODAS las 
 SELECT * FROM users WHERE first_name LIKE 'Y%' AND last_name LIKE 'C%'; -- Selecciona TODAS las columnas de la database 'users' si el dato en la columna first_name empieza con 'Y' Y el dato en la columna last-name empiece con 'C'--
 
 SELECT * FROM users WHERE email LIKE '%net' OR email LIKE '%arg'; -- Selecciona TODAS las columnas de la database 'users' si el dato en la columna email tiene terminación 'net' O con la terminación 'arg'--
+
+
+-- SELECT LIMIT --
+ SELECT * FROM users LIMIT 5;  -- Selecciona TODAS las columnas y las primeras 5 filas de la database 'users' 
+
+
+-- NULL --
+SELECT * FROM users WHERE email IS NULL; -- Selecciona TODAS las columnas de la database 'users' donde el dato de la columna email esta vacio --
+
+SELECT * FROM users WHERE email IS NOT NULL; -- Selecciona TODAS las columnas de la database 'users' donde el dato de la columna email NO esta vacio --
+
+-- MIN() - MAX() --
+SELECT MAX(age) FROM users; -- Imprime el valor maximo de la columna 'age' --
+
+SELECT MIN(age) FROM users; -- Imprime el valor minimo de la columna 'age' --
+
+
+-- COUNT() --
+SELECT COUNT(*) FROM users; -- Imprime la cantidad de usuarios con los datos diferentes a NULL (9) --
+
+SELECT COUNT(email) FROM users; -- Imprime la cantidad de usuarios con los datos en la columna email diferentes a NULL (7) --
+
+
+-- SUM() --
